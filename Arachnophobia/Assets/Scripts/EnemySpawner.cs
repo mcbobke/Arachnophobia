7dea2d314cf26e344b09245fcaa090a9d0466e39
achongco <achongco@uci.edu>
2015-04-11 10:42:37 -0700

    Minor change

--- Arachnophobia/Assets/Scripts/EnemySpawner.cs
+++ Arachnophobia/Assets/Scripts/EnemySpawner.cs
@@ -22,7 +22,7 @@ public class EnemySpawner : MonoBehaviour {
 	int currentTier = 0;						//Keeps track enemy tiers allowable
 	//Spawning Location
 	float xDistance = 6;						//Max distance in x direction from center to spawn
-	float yDistance = 2;						//Starting distance in sky
+	float yDistance = 7;						//Starting distance in sky
 
 	// Use this for initialization
 	void Awake () {
@@ -60,7 +60,7 @@ public class EnemySpawner : MonoBehaviour {
 			int spawnIndex = Random.Range (0, maxIndex-1);
 			if(!SpawnList[spawnIndex].activeInHierarchy){
 				float x_pos = Random.Range((float)(-1.0)*xDistance, xDistance);
-				float y_pos = 0;
+				float y_pos = yDistance;
 				float z_pos = SpawnList[spawnIndex].transform.position.z;
 				SpawnList[spawnIndex].transform.position = new Vector3(x_pos,y_pos,z_pos);
 				SpawnList[spawnIndex].SetActive(true);
