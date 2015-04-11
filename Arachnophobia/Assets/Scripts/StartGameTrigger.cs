3ba0a11a819cf8b5fb213a11bc07234c685d2a6a
achongco <achongco@uci.edu>
2015-04-11 16:00:34 -0700

    Added the spider that starts it all

---
+++ Arachnophobia/Assets/Scripts/StartGameTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameTrigger : MonoBehaviour {
+
+	//Deactivates for game start
+	public GameObject GameController;
+	public GameObject PreGame;
+
+	// Use this for initialization
+	void Awake () {
+		GameController.SetActive(false);
+		PreGame.SetActive(true);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag=="Player"){
+			GameController.SetActive(true);
+			PreGame.SetActive(false);
+			gameObject.SetActive(false);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
