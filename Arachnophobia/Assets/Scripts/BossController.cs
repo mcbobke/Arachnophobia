e7afbbc96195dbf443a12c2fa47a5de36f0e6066
akasiri <akasiri@uci.edu>
2015-04-12 13:43:13 -0700

    bosscontroller

---
+++ Arachnophobia/Assets/Scripts/BossController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossController : MonoBehaviour {
+
+	public float attackRate = 0.005f;
+
+	public GameObject webBallPrefab;
+	public GameObject armPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		if (Random.Range (0.0f, 1.0f) <= attackRate) {
+			float randomAttack = Random.Range(0.0f, 1.0f);
+		
+			if (randomAttack <= 0.33f) {
+				// SquashAttack
+			} else if (randomAttack <= 0.66f) {
+				// PincerAttack
+			} else {
+				SpawnWebBall();
+			}
+		}
+	}
+
+
+	void SpawnWebBall () {
+		Instantiate (webBallPrefab, new Vector2 (-10.40f, -3.50f), new Quaternion (0f, 0f, 0f, 0f));
+	}
+
+	void PincerAttack () {
+	}
+
+	void SquashAttack () {
+	}
+}
