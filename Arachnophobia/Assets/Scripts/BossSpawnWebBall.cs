fae39f7172c5c2f2615fc6e3f6886a69ab40dcdd
akasiri <akasiri@uci.edu>
2015-04-12 12:43:23 -0700

    pulling + incomplete weball scripts

---
+++ Arachnophobia/Assets/Scripts/BossSpawnWebBall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossSpawnWebBall : MonoBehaviour {
+
+	public Transform webBallPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+/*
+	void FixedUpdate () {
+		GameObject webBall = Transform.find ("WebBall");
+
+		if (webBall != null) {
+	
+		}
+	}
+*/
+	
+	// Update is called once per frame
+	void SpawnWebBall (float speed) {
+
+		if (Random.Range (0.0f, 1.0f) <= 0.5) {
+			Object webBall = Instantiate(webBallPrefab, Vector3(-10.40, -3.20, 0), Quaternion.identity);
+
+			webBall.rigidBody.
+		}
+
+	}
+}
---
+++ Arachnophobia/Assets/Scripts/WebBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebBall : MonoBehaviour {
+
+/*
+	public float speed = 2.5f;
+	public int direction = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+*/
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		if (coll.gameObject.tag == "Boundary") {
+			Destroy(this.gameObject);
+		}
+	}
+}
