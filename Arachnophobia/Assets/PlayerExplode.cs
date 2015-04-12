c613dc10cf0de4ad3a457b9d504d320778e4d5d3
achongco <achongco@uci.edu>
2015-04-12 13:21:31 -0700

    Explosion

--- Arachnophobia/Assets/PlayerExplode.cs
+++ Arachnophobia/Assets/PlayerExplode.cs
@@ -6,13 +6,19 @@ public class PlayerExplode : MonoBehaviour {
 
 	void Awake(){
 		gc = GameObject.FindGameObjectWithTag("GameController");
+		StartCoroutine("Clear");
 	}
-
+	
 	void OnTriggerStay2D(Collider2D other){
 		if(other.tag != "Player"){
 			other.gameObject.SetActive(false);
 			gc.GetComponent<EnemySpawner>().active -=1;
 		}
+	}
+
+	IEnumerator Clear(){
+		yield return new WaitForSeconds(.5f);
+		Destroy(this.gameObject,0f);
 	}
 
 }
